53eefb580288c1660caf3c26223fddaf51c2dc24
Samandar Bohodirov <91775043+samandar-dev@users.noreply.github.com>
2022-03-12 16:56:39 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// let elMenuBtn = document.getElementById('menuBtn');
+// let elMenuBox = document.getElementById('menuBox');
+// let elMenuRemov = document.getElementById('menuRemov')
+
+// elMenuBtn.addEventListener('click', function () {
+//   elMenuBox.classList.add('open-menu');
+// })
+
+// elMenuRemov.addEventListener('click', function () {
+//   elMenuBox.classList.remove('open-menu');
+// })
